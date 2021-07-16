5dc971d511037491db84c48d6bfa83ab3216d3a3
soyoungkorea <soyoung06719@gmail.com>
2021-07-16 15:16:45 +0900

    [0716]re

---
+++ js/custom_page.js
@@ -0,0 +1,113 @@
+$(function () {
+    $(".nav-button").click(function () {
+        $("body").toggleClass("nav-open");
+    });
+});
+
+$(function () {
+    var nav = $("#header-pc, #header-mo");
+    langBtn = $("button.dropdown");
+    langBtnFont = $(".dropdown .dropdown-link");
+    logo = $(".logo");
+    navItem = $("#header-pc .nav-link");
+    searchBtn = $(".icofont-search-1");
+    menuBtn = $("#header-mo .nav-button>span");
+
+    $(window).on('scroll', function () {
+
+        if ($(window).scrollTop() >= 50) {
+            nav.addClass("nav-scroll");
+            langBtn.css("background-color", "#fff");
+            langBtnFont.css("color", "#005cb9");
+            navItem.css("color", "#fff");
+            searchBtn.css("color", "#fff");
+            menuBtn.css("background-color", "#fff");
+            logo.css("fill", "#fff");
+
+        } else {
+            nav.removeClass("nav-scroll");
+            langBtn.css("background-color", "#005cb9");
+            langBtnFont.css("color", "#fff");
+            navItem.css("color", "#222");
+            searchBtn.css("color", "#222");
+            menuBtn.css("background-color", "#222");
+            logo.css("fill", "#222");
+        }
+    });
+});
+
+$(document).ready(function () {
+
+    $(".filter-button").click(function () {
+        var value = $(this).attr('data-filter');
+
+        if (value == "all") {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        } else {
+            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.' + value).hide('3000');
+            $('.filter').filter('.' + value).show('3000');
+        }
+    });
+
+});
+
+$('.thumbnail').click(function(){
+  	$('.modal-body').empty();
+  	var title = $(this).parent('a').attr("title");
+  	$('.modal-title').html(title);
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+    $('#photoModal').modal({ show: true });
+});
+
+
+// $('#next-btn').click(function() {
+//  var link = $('.modal-body a');
+//   var number = parseInt(link.attr('title').match(/\S+$/));
+//   number++;
+//   if(number === 13) {
+// 	number = 1;
+//   }
+//   $('.modal-body').html($('#img-container').find('a[title="Image ' + number + '"]').parent('div').html());
+//   $('.modal-title').text('Image ' + number);
+// });
+
+// $('#prev-btn').click(function() {
+//  var link = $('.modal-body a');
+//   var number = parseInt(link.attr('title').match(/\S+$/));
+//   number--;
+//   if(number === 0) {
+// 	number = 12;
+//   }
+//   $('.modal-body').html($('#img-container').find('a[title="Image ' + number + '"]').parent('div').html());
+//   $('.modal-title').text('Image ' + number);
+// });
+
+
+$(function () {
+    $(".support-link").click(function () {
+        $(".email_template").not("#emailForm" + $(this).attr("target")).hide();
+        $("#emailForm" + $(this).attr("target")).toggle();
+    });
+});
+
+// const req = new XMLHttpRequest();
+// req.open("GET", "/json/photo_list.json");
+// req.onreadystatechange = function () {
+//     if (this.readyState == 4) {
+//         // console.log(this.response);
+//         const data = JSON.parse(this.response);
+//         for (let i = 0; i < data.length; i++) {
+//             const div = document.createElement("div");
+//             div.setAttribute("class", "image");
+//             const img = document.createElement("img");
+//             img.src = data[i];
+//             div.appendChild(img);
+//             document.querySelector(".photo-img").appendChild(div);
+//         }
+//     }
+// }
+// req.send();
+
