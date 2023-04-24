08c4372664e8a6db3a53babd0c157170ca5fd025
sh0402 <soyoung06719@gmail.com>
2023-04-24 11:51:56 +0900

    230424

--- js/custom.js
+++ js/custom.js
@@ -42,3 +42,30 @@ $(document).ready(function () {
     })
 });
 
+
+  const [mouseDownClientX, setMouseDownClientX] = useState(0);
+  const [mouseDownClientY, setMouseDownClientY] = useState(0);
+  const [mouseUpClientX, setMouseUpClientX] = useState(0);
+  const [mouseUpClientY, setMouseUpClientY] = useState(0);
+
+const onMouseDown = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setMouseDownClientX(e.clientX);
+    setMouseDownClientY(e.clientY);
+  };
+  const onMouseUp = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setMouseUpClientX(e.clientX);
+    setMouseUpClientY(e.clientY);
+  };
+  useEffect(() => {
+    const dragSpaceX = Math.abs(mouseDownClientX - mouseUpClientX);
+    const dragSpaceY = Math.abs(mouseDownClientY - mouseUpClientY);
+    const vector = dragSpaceX / dragSpaceY;
+
+    if (mouseDownClientX !== 0 && dragSpaceX > 100 && vector > 2) {
+      if (mouseUpClientX < mouseDownClientX) {
+        handleNextBtn();
+      } else if (mouseUpClientX > mouseDownClientX) {
+        handlePrevBtn();
+      }
+    }
+  }, [mouseUpClientX]);
