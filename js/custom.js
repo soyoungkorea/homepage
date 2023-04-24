5d36b0788cd061eb5e022440bd544a0edc064a53
sh0402 <soyoung06719@gmail.com>
2023-04-24 14:03:19 +0900

    230424

--- js/custom.js
+++ js/custom.js
@@ -42,24 +42,3 @@ $(document).ready(function () {
     })
 });
 
-/*
-
-dependencies:
-
-//cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js
-//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js
-//cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js
-
-*/
-
-$(".carousel").swipe({
-
-  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-    if (direction == 'left') $(this).carousel('next');
-    if (direction == 'right') $(this).carousel('prev');
-
-  },
-  allowPageScroll:"vertical"
-
-});
