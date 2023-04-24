00fd975b15ec13c543f4e3b5a8b006235a4b85b3
sh0402 <soyoung06719@gmail.com>
2023-04-24 12:28:00 +0900

    230424

--- js/custom.js
+++ js/custom.js
@@ -42,10 +42,3 @@ $(document).ready(function () {
     })
 });
 
-$(".carousel").swipe({
-                swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-                    if (direction == 'left') $(this).carousel('next');
-                    if (direction == 'right') $(this).carousel('prev');
-                },
-                allowPageScroll: "vertical" 
-            });
